e2605dec8a33a8655bb0209702634056e8d667f5
Joel Fentes <82660102+JoelFentes@users.noreply.github.com>
2024-05-08 17:33:13 -0300

    testando chakra.ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import * as React from 'react'
 import './App.css'
-import { Input } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <label htmlFor="">E-mail</label>
-      <ChakraBaseProvider theme={theme}>
-        <Input variant='outline' placeholder='Outline' />
-      </ChakraBaseProvider>
-    </>
+    <div className='App'>
+      <Box>
+        Digite seu E-mail
+      </Box>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App'
 const rootElement = document.getElementById('root')
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
