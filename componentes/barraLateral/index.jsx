5883c9214d081157d2bfa6294089f5aaf4c01b46
Marcela Ellen <111468086+MarcelaEllen@users.noreply.github.com>
2024-05-08 18:38:39 -0300

    Update index.jsx

--- componentes/barraLateral/index.jsx
+++ componentes/barraLateral/index.jsx
@@ -6,17 +6,17 @@ function BarraLateral() {
   return (
     <div className="barra-lateral">
       <div className="logo">
-        <img src="../public/icons/infinity.png" alt="Logo" />
+        <img src="./public/icons/infinity.png" alt="Logo" />
       </div>
       <div className="icones">
         <div className="icone" id="iconeUser">
-          <img src="../public/icons/document.png" alt="Ícone 1" />
+          <img src="./public/icons/document.png" alt="Ícone 1" />
         </div>
         <div className="icone">
-          <img src="../public/icons/user.png" alt="Ícone 2" />
+          <img src="./public/icons/user.png" alt="Ícone 2" />
         </div>
         <div className="icone">
-          <img src="../public/icons/settings.png" alt="Ícone 3" />
+          <img src="./public/icons/settings.png" alt="Ícone 3" />
         </div>
       </div>
     </div>
