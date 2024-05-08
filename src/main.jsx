b77e5090ebf3a463656413c5b2da3fabb4bbe31e
Marcela <marcelaesmonteiro@gmail.com>
2024-05-08 18:27:36 -0300

    barra lateral criada

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,6 @@ import App from './App'
 const rootElement = document.getElementById('root')
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-<<<<<<< HEAD
-=======
-    <ChakraProvider theme={theme}>
->>>>>>> e2605dec8a33a8655bb0209702634056e8d667f5
       <App />
   </React.StrictMode>,
 )
