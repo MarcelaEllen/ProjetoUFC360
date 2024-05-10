295c6ccde431fe1905639faa1a9a97042a9ae44e
Joel Fentes <82660102+JoelFentes@users.noreply.github.com>
2024-05-10 16:40:12 -0300

    .

--- react-chakra-ui/src/components/avaliacaoContainer/avaliacaoContainer.tsx
+++ react-chakra-ui/src/components/avaliacaoContainer/avaliacaoContainer.tsx
@@ -32,52 +32,6 @@ export function PerguntaAvaliacao() {
     </Center>
   );
 
-  /* import { useState } from "react";
-     import { Box, Button, Flex, Input, ChakraProvider } from "@chakra-ui/react";
-
-      export function AdicionarPergunta() {
-        const [fields, setFields] = useState([{ id: 0 }]);
-
-        const addField = () => {
-          const lastField = fields[fields.length - 1];
-          const newId = lastField.id + 1;
-          setFields([...fields, { id: newId }]);
-        };
-
-        const removeField = (idToRemove) => {
-          setFields(fields.filter(field => field.id !== idToRemove));
-        };
-
-  return (
-    <ChakraProvider>
-      <Flex direction="column" position="relative" top="4vh" right="25vw">
-        <Box overflowY="auto">
-          {fields.map((field, index) => (
-            <Box display="flex" key={index} mt={4}>
-              <Input
-                position={"relative"}
-                color="#7A72FF"
-                fontSize={"1.2rem"}
-                _placeholder={{ opacity: 0.8, color: 'inherit' }}
-                variant='outline'
-                placeholder={`Opção ${field.id + 1}`}
-                w={"350px"}
-                h={"50px"}
-              />
-              {fields.length > 1 && (
-                <Button ml={2} onClick={() => removeField(field.id)} >
-                  Excluir
-                </Button>
-              )}
-            </Box>
-          ))}
-          <Button mt={4} onClick={addField}>
-            Adicionar Campo
-          </Button>
-        </Box>
-      </Flex>
-    </ChakraProvider>
-  ); */
 }
 
 
